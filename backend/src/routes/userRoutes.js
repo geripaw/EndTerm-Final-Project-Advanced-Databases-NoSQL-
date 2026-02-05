744f077ce60f774a9828b9f4e5830e753bf0f527
4everxyung <geripaw08@gmail.com>
2026-02-05 18:35:58 +0500

    Delete backend directory

--- backend/src/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { me } = require("../controllers/userController");
-const { auth } = require("../middleware/auth");
-
-router.get("/me", auth, me);
-
-module.exports = router;
